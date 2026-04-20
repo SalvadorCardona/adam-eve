229f551194ebba81d4ce137e61e10d963b946d9e
salva <hebustha@gmail.com>
2026-04-20 20:22:36 +0200

    Fix  GithubPage

--- packages/game/inventory/InventoryItemInterface.ts
+++ packages/game/inventory/InventoryItemInterface.ts
@@ -1,8 +1,6 @@
 import { BaseJsonLdItemInterface, JsonLdType } from "@/packages/jsonLd/jsonLd"
-import {
-  BaseGameResource,
-  createResourceGame,
-} from "@/packages/game/BaseGameResource"
+import { BaseGameResource, createResourceGame } from "@/packages/game/BaseGameResource"
+import { EntityType } from "@/packages/game/entity/EntityResourceInterface"
 
 export interface InventoryItemInterface extends BaseJsonLdItemInterface {
   quantity: number
@@ -10,8 +8,7 @@ export interface InventoryItemInterface extends BaseJsonLdItemInterface {
 
 export type InventoryItemType = JsonLdType
 
-export interface InventoryResource
-  extends BaseGameResource<InventoryItemInterface> {}
+export interface InventoryResource extends BaseGameResource<InventoryItemInterface> {}
 
 export function createResourceInventory<
   T extends BaseGameResource<InventoryItemInterface> =
@@ -19,15 +16,14 @@ export function createResourceInventory<
 >(resource: Partial<BaseGameResource> & Partial<T>): T {
   const meta = {
     "@type": "inventory",
+    entityType: EntityType.resource,
     ...resource,
   }
 
   return createResourceGame<T>(meta)
 }
 
-export type CanBeInventoryItemInterface =
-  | InventoryItemType
-  | BaseJsonLdItemInterface
+export type CanBeInventoryItemInterface = InventoryItemType | BaseJsonLdItemInterface
 
 export interface InventoryItemRequest {
   quantity?: number
